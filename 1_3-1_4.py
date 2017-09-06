29291aaf51cc60e5226da26bfecef1b4345b8bef
EelkeHoffman <31701848+EelkeHoffman@users.noreply.github.com>
2017-09-06 19:35:35 +0100

    1_3 en 1_4

---
+++ 1_3-1_4.py
@@ -0,0 +1,10 @@
+a = 6
+b = 7
+c = (a + b) / 2
+inventaris = ("papier", "nietjes", "pennen")
+voornaam = "Eelke"
+tussenvoegsel = ()
+achternaam = "Hoffman"
+mijnnaam = 6.75 > a, b < 6.75, inventaris == "",  len(inventaris) >= 10
+test = len(inventaris) == 5 * len(mijnnaam)
+print(mijnnaam)
