e70a61d057ffada6c0996f68ee1452c0d4ab4b38
EelkeHoffman <31701848+EelkeHoffman@users.noreply.github.com>
2017-09-06 19:49:22 +0100

    2_4

---
+++ 2_4.py
@@ -0,0 +1,3 @@
+AantalUur = input("hoeveel uur heb je gewerkt?")
+UurSalaris = input("hoeveel verdien je per uur?")
+print(float(AantalUur), "uur werken levert", float(AantalUur) * float(UurSalaris), "euro op")
